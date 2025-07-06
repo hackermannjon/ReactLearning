5ad0949598ff3a7096ccef3f1a81e865bfaafcd8
JOAO VICTOR ALVES DOS SANTOS <hyperlobe2@gmail.com>
2025-07-06 12:36:16 -0300

    feat? Display housing location details in DetailsComponent

--- homes-app/src/app/details/details.component.ts
+++ homes-app/src/app/details/details.component.ts
@@ -1,20 +1,45 @@
 import { CommonModule } from "@angular/common";
 import { Component, inject } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
+import { HousingLocation } from "../housing-location";
+import { HousingService } from "../housing.service";
 
 @Component({
 	selector: "app-details",
 	standalone: true,
 	imports: [CommonModule],
-	template: ` <p>details works! {{ housingLocationId }}</p> `,
+	template: `
+		<article>
+			<img
+				class="listing-photo"
+				[src]="housingLocation?.photo"
+				alt="Exterior photo of {{ housingLocation?.name }}"
+			/>
+			<section class="listing-description">
+				<h2 class="listing-heading">{{ housingLocation?.name }}</h2>
+				<p class="listing-location">
+					{{ housingLocation?.city + ", " + housingLocation?.state }}
+				</p>
+				<p class="listing-available-units">
+					Available Units: {{ housingLocation?.availableUnits }}
+				</p>
+			</section>
+		</article>
+	`,
 	styleUrls: ["./details.component.css"],
 })
 export class DetailsComponent {
 	route: ActivatedRoute = inject(ActivatedRoute);
+	housingService = inject(HousingService);
 	housingLocationId: number = 0;
+	housingLocation: HousingLocation | undefined;
 
 	constructor() {
-		this.housingLocationId = Number(this.route.snapshot.paramMap.get("id"));
-		console.log("Housing Location ID:", this.housingLocationId);
+		const housingLocationId = Number(
+			this.route.snapshot.paramMap.get("id")
+		);
+
+		this.housingLocation =
+			this.housingService.getHousingLocationById(housingLocationId);
 	}
 }
