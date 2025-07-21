b5365a8a6e8cab4ec1f1d7fc42b260d75f2a1df3
JOAO VICTOR ALVES DOS SANTOS <hyperlobe2@gmail.com>
2025-07-21 20:17:54 -0300

    feat: Add backend project and remove deleted files

--- Angular/homes-app/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { bootstrapApplication } from "@angular/platform-browser";
-import { provideRouter } from "@angular/router";
-import { AppComponent } from "./app/app.component";
-import routeConfig from "./app/routes";
-
-bootstrapApplication(AppComponent, {
-	providers: [provideRouter(routeConfig)],
-}).catch((err) => console.error(err));
---
+++ interview/backend/src/app.module.ts
@@ -0,0 +1,32 @@
+// Arquivo: backend/src/app.module.ts
+
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+// Valida as variáveis de ambiente essenciais antes de iniciar o módulo
+const requiredEnvVars = ['DB_HOST', 'DB_PORT', 'DB_USER', 'DB_PASSWORD', 'DB_DATABASE'];
+for (const envVar of requiredEnvVars) {
+  if (!process.env[envVar]) {
+    throw new Error(`Erro Crítico: A variável de ambiente ${envVar} não foi definida.`);
+  }
+}
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT!, 10),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      autoLoadEntities: true,
+      synchronize: true, // Em desenvolvimento, cria as tabelas automaticamente.
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
